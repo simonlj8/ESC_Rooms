2ff515633ada797186524ba18851a2bb022ffdbf
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2021-11-15 07:56:20 +0100

    add modal

---
+++ 02-esc-website/book-challenge.js
@@ -0,0 +1,27 @@
+let btns = document.querySelectorAll('.challenge-cta');
+
+btns.forEach(function (i) {
+  i.addEventListener('click', function() {
+    document.querySelector('.modal').classList.toggle('open'),
+    document.querySelector('.overlay').classList.toggle('active');
+  });
+});
+
+document.querySelector('.modal-btn').addEventListener('click', () => {
+    let valueDate = document.getElementById('date').value;
+
+    if (!valueDate) {
+        alert("Input must not be empty");
+        return false;
+    }
+    else {
+        document.querySelector('.modal-step1').classList.toggle('close', true),
+        document.querySelector('.modal-step2').classList.toggle('open', true);
+    }
+});
+
+document.querySelector('.modal-btn2').addEventListener('click', () => {
+    document.querySelector('.modal-step2').classList.toggle('close'),
+    document.querySelector('.modal-step3').classList.toggle('open');
+});
+
