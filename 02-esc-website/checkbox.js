24de698e94bdf36be0e822f25156e43df791ed15
mhema42 <mhema@telia.com>
2021-11-16 14:12:27 +0100

    filter section styled and star rating function ok

--- 02-esc-website/checkbox.js
+++ 02-esc-website/checkbox.js
@@ -1,30 +1,37 @@
-/* function () {
-    function checkbox() {
-        let rating_from = document.getElementById("challenges-list");
-        for (let i = 0; i < challenge.length; i++) {
-          let item = `<li class="challenges-item">
-} */
 
-/* function check_check()
-{
-    if (document.getElementById('rat05').checked) 
-    {
-        const cbs = document.querySelectorAll('input[name="rating_from"]');
-        cbs.forEach((cb) => {
-            cb.checked = checked;
+        const checkbox_from = Array.from(
+            document.querySelectorAll('#rating_from [type="checkbox"]')
+        ).reverse();
+
+        let lastSelected1 = -1;
+        deselectFrom = (a, b) => {
+            for (let j = a; j < b; j++) {
+                checkbox_from[j].checked = false;
+            }
+        };
+        deselectAll = () => {
+            deselectFrom(0, checkbox_from.length);
+        };
+        
+        checkbox_from.forEach((checkbox, i) => {
+            checkbox.onclick = function (event) {
+                const self = event.target;
+
+                if (self.checked) {
+                    //Select everything up to and including self
+                    deselectAll();
+                    for (let j = 0; j <= i; j++) {
+                        checkbox_from[j].checked = true;
+                    }
+                } else if (i === lastSelected1) {
+                    deselectAll();
+                } else {
+                    // Keep self checked, but deselect everything after self
+                    self.checked = true;
+                    deselectFrom(i + 1, checkbox_from.length);
+                }
+
+                lastSelected1 = i;
+                document.querySelector('#rate_from').innerText = checkbox_from.filter(x1 => x1.checked).length
+            };
         });
-    }
-} */
-
-const checkbox = document.getElementById('rat05')
-
-/* checkbox.addEventListener('change', (event) => {
-  if (event.currentTarget.checked) { */
-
-checkbox.addEventListener(currentTarget.checked) {
-
-alert("checked");
-
-const rat01 = document.getElementById("search");
-console.log(rat01.checked);
-};
