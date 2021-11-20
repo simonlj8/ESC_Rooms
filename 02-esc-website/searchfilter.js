3e2caa5bfae5c1494390f43cd8c3276fdc9f0974
Nordwall <tobiasnordwall@gmail.com>
2021-11-20 20:34:21 +0100

    fixed so that the to start rating also updates the DOM and so the inputdata is not case sensitive with .toLowerCase()

--- 02-esc-website/searchfilter.js
+++ 02-esc-website/searchfilter.js
@@ -17,15 +17,15 @@ addEventListener("load", async function loadData() {
 document.addEventListener("DOMContentLoaded", () => {
   stars_from = document.querySelectorAll(".rating_from li");
 
-  stars_from.forEach(item => {
-      item.addEventListener("click", function () {
-          let rating_from = this.getAttribute("star_rating_from");
-          ratingFrom = rating_from;
-          let data = searchData(value, challenge);
+  stars_from.forEach((item) => {
+    item.addEventListener("click", function () {
+      let rating_from = this.getAttribute("star_rating_from");
+      ratingFrom = rating_from;
+      let data = searchData(value, challenge);
 
-          renderChallenge(data);
-          return SetRatingStar(rating_from, stars_from);
-      });
+      renderChallenge(data);
+      return SetRatingStar(rating_from, stars_from);
+    });
   });
 });
 
@@ -33,62 +33,63 @@ document.addEventListener("DOMContentLoaded", () => {
 document.addEventListener("DOMContentLoaded", () => {
   stars_to = document.querySelectorAll(".rating_to li");
 
-  stars_to.forEach(item => {
-      item.addEventListener("click", function () {
-          let rating_to = this.getAttribute("star_rating_to");
-          ratingTo = rating_to;
-          return SetRatingStar(rating_to, stars_to);
-      });
+  stars_to.forEach((item) => {
+    item.addEventListener("click", function () {
+      let rating_to = this.getAttribute("star_rating_to");
+      ratingTo = rating_to;
+      let data = searchData(value, challenge);
+
+      renderChallenge(data);
+      return SetRatingStar(rating_to, stars_to);
+    });
   });
 });
 
 // render stars before the star which got "click" and return rating value */
 function SetRatingStar(rating, stars) {
   for (let i = 0; i < stars.length; i++) {
-      if (i < rating) {
-          stars[i].innerHTML = '<i class="fas fa-star"></i>';
-      } else {
-          stars[i].innerHTML = '<i class="far fa-star"></i>';
-      }
+    if (i < rating) {
+      stars[i].innerHTML = '<i class="fas fa-star"></i>';
+    } else {
+      stars[i].innerHTML = '<i class="far fa-star"></i>';
+    }
   }
-};
+}
 
-// return value to array from "type" if checked 
+// return value to array from "type" if checked
 let typeBoxes = document.querySelectorAll(".cb-type");
-let typeArray = []
+let typeArray = [];
 
 typeBoxes.forEach(function (checkbox) {
-  checkbox.addEventListener('change', function () {
-    typeArray =
-      Array.from(typeBoxes)
-        .filter(i => i.checked)
-        .map(i => i.value)
+  checkbox.addEventListener("change", function () {
+    typeArray = Array.from(typeBoxes)
+      .filter((i) => i.checked)
+      .map((i) => i.value);
 
     let data = searchData(value, challenge);
-    console.log(typeArray)
+    console.log(typeArray);
     renderChallenge(data);
-  })
+  });
 });
 
-// return value to array from "labels" if checked 
+// return value to array from "labels" if checked
 let labelBoxes = document.querySelectorAll(".cb-label");
-let labelArray = []
+let labelArray = [];
 
 labelBoxes.forEach(function (checkbox) {
-  checkbox.addEventListener('change', function () {
-    labelArray =
-      Array.from(labelBoxes)
-        .filter(i => i.checked)
-        .map(i => i.value)
+  checkbox.addEventListener("change", function () {
+    labelArray = Array.from(labelBoxes)
+      .filter((i) => i.checked)
+      .map((i) => i.value);
 
     let data = searchData(value, challenge);
 
-    console.log(labelArray)
+    console.log(labelArray);
     renderChallenge(data);
-  })
+  });
 });
 
-// key word filter 
+// key word filter
 inputData = document.getElementById("inputSearch");
 inputData.addEventListener("keyup", function () {
   let data = searchData(value, challenge);
@@ -96,23 +97,24 @@ inputData.addEventListener("keyup", function () {
   renderChallenge(data);
 });
 
-// run filter 
+// run filter
 function searchData(value, data) {
   let filteredData = [];
 
   for (var i = 0; i < data.length; i++) {
-    let value = inputData.value;
+    let value = inputData.value.toLowerCase();
+
     let title = data[i].title.toLowerCase();
     let description = data[i].description.toLowerCase();
     let labels = data[i].labels;
     let rating = data[i].rating;
     let type = data[i].type;
 
-   console.log(ratingFrom)
+    console.log(ratingFrom);
 
     if (rating >= ratingFrom && rating <= ratingTo) {
       if (type.includes(typeArray) || typeArray.length == 2) {
-        if (labelArray.every(v => labels.includes(v))) {
+        if (labelArray.every((v) => labels.includes(v))) {
           if (title.includes(value) || description.includes(value)) {
             filteredData.push(data[i]);
           }
@@ -120,7 +122,7 @@ function searchData(value, data) {
       }
     }
   }
-  console.log(filteredData)
+  console.log(filteredData);
   return filteredData;
 }
 
@@ -163,16 +165,14 @@ function renderChallenge(data) {
 // mobile menu
 document.querySelector(".main-nav-toggle").addEventListener("click", () => {
   document.querySelector(".main-nav").classList.toggle("open");
-
 });
 
-// toggle filters 
-document.querySelector('.filter-btn').addEventListener('click', () => {
-      document.querySelector('.filter-btn').classList.toggle('close'),
-      document.querySelector('#search').classList.toggle('open');
-  });
-  document.querySelector('.close-filter').addEventListener('click', () => {
-    document.querySelector('.filter-btn').classList.remove('close'),
-    document.querySelector('#search').classList.remove('open');
+// toggle filters
+document.querySelector(".filter-btn").addEventListener("click", () => {
+  document.querySelector(".filter-btn").classList.toggle("close"),
+    document.querySelector("#search").classList.toggle("open");
 });
-
+document.querySelector(".close-filter").addEventListener("click", () => {
+  document.querySelector(".filter-btn").classList.remove("close"),
+    document.querySelector("#search").classList.remove("open");
+});
