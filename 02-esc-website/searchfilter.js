c10d6db80d5b43a81d7746159027cfc91e608fdd
emmaamber1993 <89987981+emmaamber1993@users.noreply.github.com>
2021-11-20 15:50:04 +0100

    filter function fixed

--- 02-esc-website/searchfilter.js
+++ 02-esc-website/searchfilter.js
@@ -1,64 +1,132 @@
+let value;
+let challenge;
+ratingFrom = 0;
+ratingTo = 5;
+
 addEventListener("load", async function loadData() {
-  const url = "https://lernia-sjj-assignments.vercel.app/api/challenges";
-  const response = await fetch(url);
+  challenges = "https://lernia-sjj-assignments.vercel.app/api/challenges";
+  const response = await fetch(challenges);
   const data = await response.json();
 
-  window.challenge = data.challenges;
-
+  console.log(data);
+  challenge = data.challenges;
   renderChallenge(challenge);
 });
 
+// starrating from
+document.addEventListener("DOMContentLoaded", () => {
+  stars_from = document.querySelectorAll(".rating_from li");
 
-checkboxes = document.querySelectorAll("input[type=checkbox]");
-checkboxes.forEach(item => {
-    item.addEventListener("click", function() {
-      console.log("test")
-      let value = "";
-      let data = searchData(value, challenge);
+  stars_from.forEach(item => {
+      item.addEventListener("click", function () {
+          let rating_from = this.getAttribute("star_rating_from");
+          ratingFrom = rating_from;
+          let data = searchData(value, challenge);
 
-      renderChallenge(data);
-    });
+          renderChallenge(data);
+          return SetRatingStar(rating_from, stars_from);
+      });
+  });
 });
 
+// starrating to
+document.addEventListener("DOMContentLoaded", () => {
+  stars_to = document.querySelectorAll(".rating_to li");
 
+  stars_to.forEach(item => {
+      item.addEventListener("click", function () {
+          let rating_to = this.getAttribute("star_rating_to");
+          ratingTo = rating_to;
+          return SetRatingStar(rating_to, stars_to);
+      });
+  });
+});
+
+// render stars before the star which got "click" and return rating value */
+function SetRatingStar(rating, stars) {
+  for (let i = 0; i < stars.length; i++) {
+      if (i < rating) {
+          stars[i].innerHTML = '<i class="fas fa-star"></i>';
+      } else {
+          stars[i].innerHTML = '<i class="far fa-star"></i>';
+      }
+  }
+};
+
+// return value to array from "type" if checked 
+let typeBoxes = document.querySelectorAll(".cb-type");
+let typeArray = []
+
+typeBoxes.forEach(function (checkbox) {
+  checkbox.addEventListener('change', function () {
+    typeArray =
+      Array.from(typeBoxes)
+        .filter(i => i.checked)
+        .map(i => i.value)
+
+    let data = searchData(value, challenge);
+    console.log(typeArray)
+    renderChallenge(data);
+  })
+});
+
+// return value to array from "labels" if checked 
+let labelBoxes = document.querySelectorAll(".cb-label");
+let labelArray = []
+
+labelBoxes.forEach(function (checkbox) {
+  checkbox.addEventListener('change', function () {
+    labelArray =
+      Array.from(labelBoxes)
+        .filter(i => i.checked)
+        .map(i => i.value)
+
+    let data = searchData(value, challenge);
+
+    console.log(labelArray)
+    renderChallenge(data);
+  })
+});
+
+// key word filter 
 inputData = document.getElementById("inputSearch");
 inputData.addEventListener("keyup", function () {
-  
-  let value = this.value;
   let data = searchData(value, challenge);
 
   renderChallenge(data);
 });
 
+// run filter 
 function searchData(value, data) {
   let filteredData = [];
 
   for (var i = 0; i < data.length; i++) {
-    value = value.toLowerCase();
+    let value = inputData.value;
     let title = data[i].title.toLowerCase();
     let description = data[i].description.toLowerCase();
+    let labels = data[i].labels;
     let rating = data[i].rating;
     let type = data[i].type;
-    let labels = data[i].labels;
 
-    console.log(ratingFrom)
+   console.log(ratingFrom)
 
-    if (type.includes(typeArray)) {
-    if (labelArray.every(v=> labels.includes(v))) {
     if (rating >= ratingFrom && rating <= ratingTo) {
-    if (title.includes(value) || description.includes(value)) {
-      filteredData.push(data[i]);
+      if (type.includes(typeArray) || typeArray.length == 2) {
+        if (labelArray.every(v => labels.includes(v))) {
+          if (title.includes(value) || description.includes(value)) {
+            filteredData.push(data[i]);
+          }
+        }
+      }
     }
-    } 
-    } 
-    }   
   }
+  console.log(filteredData)
   return filteredData;
 }
 
+// display challenges
 function renderChallenge(data) {
   let ul = document.getElementById("challenges-list");
-
   ul.innerHTML = "";
 
   for (let i = 0; i < data.length; i++) {
@@ -95,39 +163,8 @@ function renderChallenge(data) {
   }
 }
 
-/* for (let i = 0; i < challenge.length; i++) {
-    console.log(challenge[i].Titel);
-
-    function createli() {
-        const ul = document.getElementByclass("challenges-list");
-        const li = document.createElement("li").className("challenges-item");
-        ul.appendChild(li);
-    }
-}; 
-*/
-
-/*
-<li class="challenges-item">
- <img class="challenge-picture" src="images/challenge.png" />
- <h3 class="challenge-title">#######</h3>
- <div class="challenge-meta">
-     <ul class="challenge-rating">
-         <li class="challenge-rating-star on"></li>
-        <li class="challenge-rating-star on"></li>
-        <li class="challenge-rating-star on"></li>
-        <li class="challenge-rating-star on"></li>
-        <li class="challenge-rating-star off"></li>
-    </ul>
-    <small class="challenge-size">######</small>
- </div>
-    <p class="challenge-description">
-    #########
- </p>
-<a class="challenge-cta" href="#">Book this room</a>
-</li>
-*/
-
 // mobile menu
 document.querySelector(".main-nav-toggle").addEventListener("click", () => {
   document.querySelector(".main-nav").classList.toggle("open");
+
 });
